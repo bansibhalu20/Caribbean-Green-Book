8656a0e1229d4b3df068cab1cbc5823ec476c979
Ishalakhani <149379301+Ishalakhani@users.noreply.github.com>
2024-01-20 17:17:55 +0530

    Created New Video Contrller

--- caribbean/app/Http/Controllers/admin/AdminBusinessController.php
+++ caribbean/app/Http/Controllers/admin/AdminBusinessController.php
@@ -12,16 +12,21 @@ use Illuminate\Support\Facades\Storage;
 use RealRashid\SweetAlert\Facades\Alert; 
 class AdminBusinessController extends Controller
 {
-    public function create()
-    {
-        $data['name']=Country::all();
-        $data['category']=Category::all();
-        $data['plan']=Plan::all();
-        return view('admin.business.add',$data);
-    }
+   // Display the form to create a new business
+   public function create()
+   {
+       // Fetch data for dropdowns: countries, categories, and plans
+       $data['name'] = Country::all();
+       $data['category'] = Category::all();
+       $data['plan'] = Plan::all();
+       return view('admin.business.add', $data);
+   }
+    // Store a new business in the database
     public function store(Request $request)
     {
+         // Create a new Business model instance
         $business = new Business();
+         // Assign input values to the corresponding attributes
         $business->name = $request->input('name');
         $business->email = $request->input('email');
         $business->website = $request->input('site');
@@ -40,6 +45,7 @@ class AdminBusinessController extends Controller
         $business->country_id = $request->input('country');
         $business->state = $request->input('state');
         $business->city = $request->input('city'); 
+          // Handle file upload for the business image
         if ($request->hasFile('image')) {
             $file = $request->file('image');
             $fileName = 'admin/business_image/' . $file->getClientOriginalName();   
@@ -48,27 +54,37 @@ class AdminBusinessController extends Controller
             // Update the image attribute with the file path
             $business->image = $fileName;
         }
-         
+          // Save the business to the database
         $business->save();
+         // Redirect to the business view page after successfully storing the business
         return redirect(route('admin.business-show'));
     }
 
-    public function show()
-    {
-        $businesses = Business::join('tbl_country', 'tbl_country.id', '=', 'tbl_business.country_id')
-            ->select('tbl_business.*', 'tbl_country.name as country_name')
-            ->get();
-    
-        return view('admin.business.view', compact('businesses'));
-    }
+   // Display a list of businesses
+   public function show()
+   {
+       // Retrieve businesses with associated country names
+       $businesses = Business::join('tbl_country', 'tbl_country.id', '=', 'tbl_business.country_id')
+           ->select('tbl_business.*', 'tbl_country.name as country_name')
+           ->get();
 
-    public function edit($id)
-    {
-        $busi = Business::find($id);
-        $data['countries'] = Country::all();
-        $data['categories']=Category::all();
-        $data['plan']=Plan::all();
-        return view('admin.business.edit',compact('busi','id'),$data);
-    }
+       // Display the view with the list of businesses
+       return view('admin.business.view', compact('businesses'));
+   }
+
+     // Display the form to edit a specific business
+     public function edit($id)
+     {
+         // Retrieve the business with the given ID
+         $busi = Business::find($id);
+ 
+         // Fetch data for dropdowns: countries, categories, and plans
+         $data['countries'] = Country::all();
+         $data['categories'] = Category::all();
+         $data['plan'] = Plan::all();
+ 
+         // Display the edit view with the business data and dropdown options
+         return view('admin.business.edit', compact('busi'), $data);
+     }
     
 }
