a4729aee6e9f4c06ae4c2483e7e29e663c6b310c
IshuLakhani <isha15038@gmail.com>
2024-01-22 14:25:46 +0530

    cheang link

--- caribbean/resources/views/admin/video/create.blade.php
+++ caribbean/resources/views/admin/video/create.blade.php
@@ -7,18 +7,26 @@
             <div id="kt_app_toolbar_container" class="app-container container-xxl d-flex flex-stack">
                 <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
                     <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">
-                        Business Form</h1>
+                       Video Form</h1>
 
                     <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
-                        <li class="breadcrumb-item text-muted">
-                            <a href="#" class="text-muted text-hover-primary">Dashboard</a>
+                    <li class="breadcrumb-item text-muted">
+                            <a href="{{route('adminDashboard')}}" class="text-muted text-hover-primary">Dashboard</a>
                         </li>
 
                         <li class="breadcrumb-item">
                             <span class="bullet bg-gray-400 w-5px h-2px"></span>
                         </li>
                         <li class="breadcrumb-item text-muted">
-                            <a href="#" class="text-muted text-hover-primary">Add video</a>
+                            <a href="{{route('admin.video.add')}}" class="text-muted text-hover-primary">Add video</a>
+                        </li>
+
+                        <!-- Add breadcrumb for Video Form -->
+                        <li class="breadcrumb-item">
+                            <span class="bullet bg-gray-400 w-5px h-2px"></span>
+                        </li>
+                        <li class="breadcrumb-item text-dark">
+                            Video Form
                         </li>
                     </ul>
                 </div>
